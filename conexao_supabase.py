784255184e2c0550c76fdf58c23abb8d75be527a
Rafaela Cunha <rafaela.cunha1972@gmail.com>
2026-07-26 16:43:11 -0300

    fix: ajusta conexao do supabase

--- conexao_supabase.py
+++ conexao_supabase.py
@@ -1,15 +1,25 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
+import streamlit as st
 from supabase import create_client, Client
 
 # Carrega as variáveis contidas no arquivo .env para a memória do sistema
-load_dotenv()
+#load_dotenv()
 
-URL = os.getenv("SUPABASE_URL")
-KEY = os.getenv("SUPABASE_KEY")
+#URL = os.getenv("SUPABASE_URL")
+#KEY = os.getenv("SUPABASE_KEY")
 
-if not URL or not KEY:
-    raise ValueError("Erro crítico: As variáveis SUPABASE_URL e SUPABASE_KEY não foram encontradas no arquivo .env!")
+try:
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+except (FileNotFoundError, KeyError):
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Erro crítico: As variáveis SUPABASE_URL e SUPABASE_KEY não foram encontradas!")
+
 
 # Cria o cliente global que todo o projeto vai usar
-supabase: Client = create_client(URL, KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
